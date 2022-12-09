567cd058a31728ffd10056ae35d1f6fd652c7c9c
jung0307 <116643560+jung0307@users.noreply.github.com>
2022-12-10 08:54:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+
+        System.out.println((int)ch);
+    }
+
+
+}
+
