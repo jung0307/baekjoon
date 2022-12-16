d70fcd53e16fa3d97c2ec6017567ea88d5ee6b9b
jung0307 <116643560+jung0307@users.noreply.github.com>
2022-12-16 20:42:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1056 ms, Memory: 112372 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,67 @@
+
+
+import java.io.*;
+
+public class Main {
+
+    static int[] arrInt2;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        5
+//        5
+//        4
+//        3
+//        2
+//        1
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arrInt = new int[n];
+
+        for(int i = 0; i < n; i++) {
+           int m = Integer.parseInt(br.readLine());
+            arrInt[i] = m;
+
+        }
+
+        arrInt2 = new int[arrInt.length];
+        solution(arrInt , 0 , n-1);
+        arrInt2 = null;
+
+        for(int a : arrInt){
+            bw.write(a+"\n");
+        }
+
+        bw.flush();
+
+    }
+
+    public static void solution(int[] arrInt , int l , int r) {
+
+        if(r > l){
+            int center = (l + r)/2;
+            int i;
+            int p = 0;
+            int j = 0;
+            int k = l;
+
+            solution(arrInt , l , center);
+            solution(arrInt , center+1 , r);
+
+            for(i = l; i <= center; i++) {
+                arrInt2[p++] = arrInt[i];
+            }
+            while (i <= r && j < p) {
+                arrInt[k++] = arrInt[i] >= arrInt2[j] ? arrInt2[j++] : arrInt[i++];
+            }
+            while (j < p) {
+                arrInt[k++] = arrInt2[j++];
+            }
+        }
+
+    }
+
+}
