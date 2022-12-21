9b6c860b3ca4e24e12aced97af63ff37005fce90
jung0307 <116643560+jung0307@users.noreply.github.com>
2022-12-21 20:29:56 +0900

    [Bronze III] Title: 택시 기하학, Time: 136 ms, Memory: 14484 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,21 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println(String.format("%.6f" , Math.PI*Math.pow(a ,2)));
+        System.out.println(String.format("%.6f" , 2*Math.pow(a , 2)));
+
+    }
+
+
+}
