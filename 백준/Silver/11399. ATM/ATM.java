c178ccef7a13317cf704a21670cd63b08d9c7acd
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-01-04 18:29:09 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -45,30 +45,32 @@ public class Main {
 //            prev += Integer.parseInt(s[i]);
 //        }
 //        System.out.println(sum);
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        String s[] = br.readLine().split(" ");
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int[] arr = new int[1001];
-
-
-        while (N-- > 0) {
-            arr[Integer.parseInt(st.nextToken())]++;
+        int[] arr = new int[n];
+        for(int i = 0; i < s.length; i++) {
+            arr[i] = Integer.parseInt(s[i]);
         }
 
-        int prev = 0;
+        Arrays.sort(arr);
+
+        int arrInt[] = new int[n];
+        arrInt[0] = arr[0];
+
+        for(int i = 1; i < arrInt.length; i++) {
+            arrInt[i] =  arr[i] + arrInt[i-1];
+        }
+
         int sum = 0;
-
-        for (int i = 0; i < 1001; i++) {
-
-            while (arr[i]-- > 0) {
-
-                sum += (i + prev);
-                prev += i;
-            }
+        for(int i = 0; i < arrInt.length; i++) {
+            sum = sum + arrInt[i];
         }
+
         System.out.println(sum);
 
     }
