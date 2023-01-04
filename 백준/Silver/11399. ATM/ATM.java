99721852c725149d7c78170293c0d02a7bd406aa
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-01-04 18:06:56 +0900

    [Silver IV] Title: ATM, Time: 272 ms, Memory: 19912 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+
+		Arrays.sort(arr);
+ 
+ 
+		int prev = 0;
+		int sum = 0;	
+				
+		for(int i = 0; i < N; i++) {	
+			sum += prev + arr[i];
+			prev += arr[i];
+		}
+		System.out.println(sum);
+	}
+}
