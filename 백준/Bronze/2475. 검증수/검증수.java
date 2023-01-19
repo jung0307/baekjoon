d16e32667fd8982c084d91bd2e7eb633d4f5d8ac
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-01-19 16:26:10 +0900

    [Bronze V] Title: 검증수, Time: 116 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+
+        int sum = 0;
+        for(int i = 0; i < s.length; i++) {
+            sum = sum + Integer.parseInt(s[i])*Integer.parseInt(s[i]);
+        }
+
+        System.out.println(sum%10);
+
+    }
+}
