0ce3ea41271b325cf552f8663c475734637311e5
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-02-15 19:52:50 +0900

    [Bronze IV] Title: 연세대학교, Time: 128 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if(n == 0) {
+            System.out.println("YONSEI");
+        }else {
+            System.out.println("Leading the Way to the Future");
+        }
+
+    }
+}
