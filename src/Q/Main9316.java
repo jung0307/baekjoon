349152da6ceaef4006e05d1ca8cbf70d4406bbe7
jung <jeong00015@gmail.com>
2023-03-26 22:43:54 +0900

    Hello Judge

---
+++ src/Q/Main9316.java
@@ -0,0 +1,19 @@
+package Q;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main9316 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+            System.out.println("Hello World, Judge "+i+"!");
+        }
+
+    }
+}
