be6c0900bbe167c069c5f9f9d00ae2c1a238a0ef
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-05-07 13:56:09 +0900

    [Bronze III] Title: 더하기 4, Time: 172 ms, Memory: 16220 KB -BaekjoonHub

---
+++ 백준/Bronze/11024. 더하기 4/더하기 4.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            String s[] = br.readLine().split(" ");
+            int sum = 0;
+
+            for(int j = 0; j < s.length; j++) {
+                sum = sum + Integer.parseInt(s[j]);
+            }
+
+            sb.append(sum+"\n");
+        }
+
+        System.out.print(sb.toString());
+
+    }
+}
