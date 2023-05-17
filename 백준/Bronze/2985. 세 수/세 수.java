d449b3ca1d8739b645095d54e7e7c52e7ea99562
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-05-18 03:02:42 +0900

    [Bronze III] Title: 세 수, Time: 160 ms, Memory: 16132 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,36 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+
+        if(a == b + c) {
+            System.out.println(a+"="+b+"+"+c);
+        } else if (a == b - c) {
+            System.out.println(a+"="+b+"-"+c);
+        } else if (a == b * c) {
+            System.out.println(a+"="+b+"*"+c);
+        } else if (a == b / c) {
+            System.out.println(a+"="+b+"/"+c);
+        } else if (a + b == c) {
+            System.out.println(a+"+"+b+"="+c);
+        } else if (a - b == c) {
+            System.out.println(a+"-"+b+"="+c);
+        } else if (a * b == c) {
+            System.out.println(a+"*"+b+"="+c);
+        } else if (a / b == c) {
+            System.out.println(a+"/"+b+"="+c);
+        }
+
+    }
+}
