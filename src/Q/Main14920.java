67be55b9d45a8dc86c573b6e558c033379e24af9
jung <jeong00015@gmail.com>
2023-06-07 22:43:24 +0900

    3n + 1 수열

---
+++ src/Q/Main14920.java
@@ -0,0 +1,34 @@
+package Q;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main14920 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+
+        while (true) {
+            if(n != 1) {
+                if(n%2 == 0) {
+                    n = n / 2;
+                }else {
+                    n = 3*n + 1;
+                }
+                cnt++;
+            }else {
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+
+
+
+    }
+}
