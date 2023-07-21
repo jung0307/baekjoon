5e95a611c3b67d8e948ec95e78e381a22836785c
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-07-21 22:03:13 +0900

    [Bronze IV] Title: R2, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+
+        System.out.println(2*b - a);
+    }
+}
