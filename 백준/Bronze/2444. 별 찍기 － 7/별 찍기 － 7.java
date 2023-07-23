8eaec9b5980d717d68b0eacc2f96d2ef40ecb0ae
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-07-23 20:12:40 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 460 ms, Memory: 21036 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = (2*n - 2*i)/2; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for(int j = 0; j < 2*i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+        for(int i = n - 1; i > 0; i--) {
+
+            for(int j = (2*n - 2*i)/2; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for(int j = 0; j < 2*i - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
+
+    }
+}
