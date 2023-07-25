42d7915ca8bac69643e328db0fb8d7f62c50d9f1
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-07-26 01:50:30 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 132 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+
+        int arrInt[] = {Integer.parseInt(s[0]) ,Integer.parseInt(s[1]) , Integer.parseInt(s[2]) };
+        Arrays.sort(arrInt);
+
+        System.out.println(arrInt[2] + arrInt[1]);
+
+    }
+}
