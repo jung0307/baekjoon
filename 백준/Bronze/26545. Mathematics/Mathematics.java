1d50cf8d649b9e796a89e907d701c2c18819582b
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-07-25 20:30:10 +0900

    [Bronze V] Title: Mathematics, Time: 116 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/26545. Mathematics/Mathematics.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum = sum + Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
