d17329737082063fe1aaf26c02fd76d9ad8a9d0d
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-10-23 22:03:31 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 124 ms, Memory: 14316 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+
+        int arrInt[] = {a , b , c};
+
+        Arrays.sort(arrInt);
+
+        System.out.println(2 * arrInt[2] - arrInt[0] - arrInt[1]);
+
+    }
+}
