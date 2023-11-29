df10e6125fb751f19118d9113b8ccb1aa4d1cb51
jung0307 <116643560+jung0307@users.noreply.github.com>
2023-11-29 22:06:07 +0900

    [Bronze II] Title: Grading Exams, Time: 316 ms, Memory: 26320 KB -BaekjoonHub

---
+++ 백준/Bronze/9296. Grading Exams/Grading Exams.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            br.readLine();
+
+            String s1[] = br.readLine().split("");
+            String s2[] = br.readLine().split("");
+
+            int cnt = 0;
+
+            for(int j = 0; j < s1.length; j++) {
+                if(!s1[j].equals(s2[j])) {
+                    cnt++;
+                }
+            }
+
+            sb.append("Case " + (i + 1) + ": " + cnt + "\n");
+        }
+
+        System.out.print(sb.toString());
+
+    }
+}
