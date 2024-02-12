9684bc2e0b51bc2148926cd10535457b1a900b1e
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-02-12 19:13:06 +0900

    [Silver II] Title: N과 M (12), Time: 204 ms, Memory: 18676 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int arr[];
+    static boolean v[];
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
+        arr = new int[N + 1];
+        v = new boolean[N + 1];
+
+        String s2[] = br.readLine().split(" ");
+
+        for(int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(s2[i - 1]);
+        }
+
+        Arrays.sort(arr);
+        solution(M , "");
+
+        Iterator<String> ir = set.iterator();
+
+        while (ir.hasNext()) {
+            sb.append(ir.next()+"\n");
+        }
+
+        System.out.print(sb.toString());
+
+    }
+
+    static void solution(int m , String ss) {
+        if(m == 0) {
+            set.add(ss);
+            return;
+        }
+
+        for(int i = 1; i <= N; i++) {
+            if(v[i] == false) {
+                solution(m - 1 , ss + arr[i] + " ");
+                v[i] = true;
+                for(int j = i + 1; j <= N; j++) {
+                    v[j] = false;
+                }
+            }
+        }
+    }
+}
