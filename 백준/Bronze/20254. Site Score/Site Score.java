873c13a68be6b01815340ec3cb1b4285b8013dd8
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-02-25 19:06:10 +0900

    [Bronze V] Title: Site Score, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+        int d = Integer.parseInt(s[3]);
+
+        System.out.println(56 * a + 24 * b + 14 * c + 6 * d);
+
+    }
+}
