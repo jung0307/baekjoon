c5f18dd659235fe1ed2ba53f7120023f9b6ef662
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-03-19 17:16:38 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 136 ms, Memory: 15384 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String s = br.readLine();
+
+        System.out.println(s.substring(n - 5));
+
+
+
+    }
+}
