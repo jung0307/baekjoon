60828bb092c8e7c34f2a9d12c0f995241fded9d7
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-04-30 21:07:49 +0900

    [Unrated] Title: SHA-224, Time: 152 ms, Memory: 15784 KB -BaekjoonHub

---
+++ 백준/Unrated/10929. SHA－224/SHA－224.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-224");
+            byte[] hash = md.digest(s.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if(hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            System.out.println(hexString.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
