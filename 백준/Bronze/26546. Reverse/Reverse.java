92f457ab25ecdb56d0c83dd180669b5a07b16ada
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-05-04 18:32:08 +0900

    [Bronze IV] Title: Reverse, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/26546. Reverse/Reverse.java
+++ 백준/Bronze/26546. Reverse/Reverse.java
@@ -7,23 +7,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < n; i++) {
             String s[] = br.readLine().split(" ");
             int a = Integer.parseInt(s[1]);
             int b = Integer.parseInt(s[2]);
+
             String ss[] = s[0].split("");
 
             for(int j = 0; j < ss.length; j++) {
-                if(!(j >= a && j < b)) {
+                if(j < a || j >= b) {
                     sb.append(ss[j]);
                 }
             }
+
             sb.append("\n");
         }
 
         System.out.print(sb.toString());
+
     }
 }
