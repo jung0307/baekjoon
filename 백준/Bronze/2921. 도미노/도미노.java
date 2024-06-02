10117bbf47785091d7422f84e38898ec0c87654a
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-06-02 20:41:54 +0900

    [Bronze III] Title: 도미노, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2921. 도미노/도미노.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private void s() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                sum += i;
+                sum += j;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().s();
+    }
+}
