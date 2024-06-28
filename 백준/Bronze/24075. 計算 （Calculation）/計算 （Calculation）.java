e0c26d946fb77bf44d7ebf31fb2e677ae5dd44fc
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-06-28 22:20:36 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 128 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s[] = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+
+        int aPb = a + b;
+        int aMb = a - b;
+
+        if(aPb >= aMb) {
+            System.out.print(aPb + "\n" + aMb);
+        }else {
+            System.out.print(aMb + "\n" + aPb);
+        }
+
+
+    }
+}
