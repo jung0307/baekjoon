a5770bd3a9ed484d32b1b99d88440d54ad45a0ae
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-07-02 22:45:21 +0900

    [Bronze IV] Title: 桁 (Digit), Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/31615. 桁 （Digit）/桁 （Digit）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        String s = (A + B) + "";
+        
+        System.out.println(s.length());
+        
+        
+    }
+}
