d0b31620eb87450648a8a096c6814e0a3f88864e
jung0307 <116643560+jung0307@users.noreply.github.com>
2024-12-28 23:34:35 +0900

    [Silver II] Title: 친구, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Silver/1058. 친구/친구.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+
+		char[][] friend = new char[N][N];
+		boolean[][] isFriend = new boolean[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < N; j++) {
+				friend[i][j] = str.charAt(j);
+			}
+		}
+
+		int max = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < N; i++) {
+			int twoFriend = 0;
+			for (int j = 0; j < N; j++) {
+				if(friend[i][j]=='Y') {
+					isFriend[i][j] = true;
+					for (int k = 0; k < N; k++) {
+						if(friend[j][k]=='Y') isFriend[i][k] = true;
+					}
+				}
+			}
+			
+			for (int j = 0; j < N; j++) {
+				if(i==j) continue;
+				if(isFriend[i][j]) twoFriend++;
+			}
+			 
+			max = Math.max(max, twoFriend);
+		}
+		
+		System.out.println(max);
+
+	}
+
+}
